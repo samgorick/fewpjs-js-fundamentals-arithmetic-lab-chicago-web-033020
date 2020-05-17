3c9ee1c11305e14225eac29446fab6dde88d2b48
gozmeister <samgorick.sg@gmail.com>
2020-05-17 14:17:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const random = Math.round(Math.random() * 100)
 
-// Write your code here
+const num1 = 62
+const num2 = 1
+
+const multiply = num1 * num2
+
+const mod = 14 % 5
+
+const max = Math.max(1, 2, 3, 4, 5, 20)
